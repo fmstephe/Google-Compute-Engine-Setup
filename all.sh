8733a10d57675f28a67e943ccb3fbcee5c87fd1c
Francis Stephens <francisstephens@gmail.com>
2014-12-02 12:13:19 +0000

    Script for installing Google Compute Engine SDK

---
+++ all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d ~/google-cloud-sdk ]
+	echo "Google Cloud SDK already installed"
+	exit
+fi
+wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz
+tar xfv google-cloud-sdk.tar.gz ~/
+rm google-cloud-sdk.tar.gz
+cd ~/google-cloud-sdk
+./install.sh
+cd -
